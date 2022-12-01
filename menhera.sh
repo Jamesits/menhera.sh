076c9825fd90a1bec77c3117c4dc28d269767684
unknowndevQwQ <unknowndevQwQ@pm.me>
2022-12-01 21:07:48 +0800

    fix: use list-units not list-unit-files
    systemd 在根目录切换到新的 rootfs 并 reload 后，list-unit-files 展示的内容是新 rootfs 提供的
    
    另一种可能的解决方式是停止 sshd 后daemon-reload
    
    fix #18

--- menhera.sh
+++ menhera.sh
@@ -34,7 +34,7 @@ EOF
 }
 
 menhera::__compat_sshd_name() {
-    if systemctl list-unit-files sshd.service >/dev/null; then
+    if systemctl list-units ssh.service | grep "0 loaded" >/dev/null; then
         echo sshd
     else
         echo ssh
